17d9e8b9d5a33c9ea68c42957a79b49fc06a37af
Owendho <36518961+Owendho@users.noreply.github.com>
2020-02-03 21:16:26 +0100

    Add project files.

---
+++ Sorting aldorithym/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Sorting_aldorithym
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.lstBox2 = new System.Windows.Forms.ListBox();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.btnOne = new System.Windows.Forms.Button();
+            this.NumberGenerator = new System.Windows.Forms.Button();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lstBox
+            // 
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.ItemHeight = 16;
+            this.lstBox.Location = new System.Drawing.Point(12, 25);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(242, 388);
+            this.lstBox.TabIndex = 0;
+            // 
+            // lstBox2
+            // 
+            this.lstBox2.FormattingEnabled = true;
+            this.lstBox2.ItemHeight = 16;
+            this.lstBox2.Location = new System.Drawing.Point(545, 25);
+            this.lstBox2.Name = "lstBox2";
+            this.lstBox2.Size = new System.Drawing.Size(242, 388);
+            this.lstBox2.TabIndex = 1;
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(107, 443);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(147, 22);
+            this.txtBox1.TabIndex = 2;
+            this.txtBox1.TextChanged += new System.EventHandler(this.txtBox1_TextChanged);
+            // 
+            // btnOne
+            // 
+            this.btnOne.Location = new System.Drawing.Point(327, 25);
+            this.btnOne.Name = "btnOne";
+            this.btnOne.Size = new System.Drawing.Size(161, 81);
+            this.btnOne.TabIndex = 3;
+            this.btnOne.Text = "bubbleSort";
+            this.btnOne.UseVisualStyleBackColor = true;
+            this.btnOne.Click += new System.EventHandler(this.btnOne_Click);
+            // 
+            // NumberGenerator
+            // 
+            this.NumberGenerator.Location = new System.Drawing.Point(327, 125);
+            this.NumberGenerator.Name = "NumberGenerator";
+            this.NumberGenerator.Size = new System.Drawing.Size(161, 70);
+            this.NumberGenerator.TabIndex = 4;
+            this.NumberGenerator.Text = "Generate";
+            this.NumberGenerator.UseVisualStyleBackColor = true;
+            this.NumberGenerator.Click += new System.EventHandler(this.NumberGenerator_Click);
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Location = new System.Drawing.Point(599, 443);
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(147, 22);
+            this.txtBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 548);
+            this.Controls.Add(this.txtBox2);
+            this.Controls.Add(this.NumberGenerator);
+            this.Controls.Add(this.btnOne);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.lstBox2);
+            this.Controls.Add(this.lstBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstBox;
+        private System.Windows.Forms.ListBox lstBox2;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.Button btnOne;
+        private System.Windows.Forms.Button NumberGenerator;
+        private System.Windows.Forms.TextBox txtBox2;
+    }
+}
+
---
+++ Sorting aldorithym/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Sorting_aldorithym
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[] MitArray;
+
+
+
+
+
+
+
+   
+
+        Random randomnumber = new Random();
+
+
+        private void btnOne_Click(object sender, EventArgs e)
+        {
+            /*int Min = 1;
+            int Max = 9;
+            */
+            
+            int temp;
+
+           
+
+
+            int n = int.Parse(txtBox1.Text);
+            MitArray = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                MitArray[i] = randomnumber.Next(1, generate);
+            }
+            
+
+            
+
+
+            /*for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = randomnumber.Next(Min, Max);
+            }
+            */
+
+
+
+            /*foreach (int n in array)
+            {
+                randomnumber.Next(Min, Max);
+            }
+
+
+
+            //int[] array = new int[randomNumber];
+            n = int.Parse(txtBox1.Text);
+                 
+            //int num = random.Next(1, 9);
+            
+
+            int[] array = new int[n];
+
+            /*
+            Array.Resize(ref array, array.Length + 1);
+            array[array.GetUpperBound(0)] = num;
+            */
+
+
+           /* for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if(array[j] > array[i + 1])
+                    {
+                        temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+
+                    }
+
+                }
+
+                
+            }
+
+            foreach (int item in array)
+            {
+                lstBox.Items.Add(item);
+
+            }*/
+
+
+
+
+
+       
+
+
+
+        }
+
+        private void txtBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumberGenerator_Click(object sender, EventArgs e)
+        {
+            int generate = int.Parse(txtBox2.Text);
+            int rnd = randomnumber.Next(1, generate);
+        }
+    }
+}
